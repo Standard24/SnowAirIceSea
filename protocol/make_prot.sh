d853fe79292d0127dc2e3604436bfaa9ff928e6d
Moritz <bitterling@quantentunnel.de>
2018-03-21 21:49:51 +0100

    Fixed bibliography.

--- protocol/make_prot.sh
+++ protocol/make_prot.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-pdflatex MainPart.tex
-#bibtex Mainpart.aux
-pdflatex MainPart.tex
-#pdflatex MainPart.tex
+pdflatex -output-directory=./bin MainPart.tex
+
+
+# ok, this is stupid: bibtex has to be executed in ./bin now.
+# so, go there, take some bibliography files with you and delete them after you used it
+cp AGFstyle.bst literature.bib ./bin
+cd  ./bin
+bibtex MainPart.aux
+rm AGFstyle.bst literature.bib
+cd ../
+
+pdflatex -output-directory=./bin MainPart.tex
+pdflatex -output-directory=./bin MainPart.tex
