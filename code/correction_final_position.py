05f6bec526ab5a75662033eef6fd4b23192c5f34
lindathielke <linda-thielke@gmx.de>
2018-04-07 12:33:11 +0200

    extension of the script for the final corrected data and latex table files

--- code/correction_final_position.py
+++ code/correction_final_position.py
@@ -9,8 +9,8 @@ import numpy as np
 
 # --- read in post processed data
 # TBC data
-filename = '2018_stake_coordinates_trimble_post'
-data_tbc = pd.read_csv('../data/stake_coordinates/' + filename
+filename_tbc = '2018_stake_coordinates_trimble_post'
+data_tbc = pd.read_csv('../data/stake_coordinates/' + filename_tbc
         + '.csv', sep=' ')
       
 data_tbc_dir = {}
@@ -19,14 +19,14 @@ for i in range(0, len(data_tbc['Name'])):
     'Easting': data_tbc['Easting'][i], 'Elevation': data_tbc['Elevation'][i]}
     
 # open source data 
-#filename = '2018_stake_coordinates_open_source_post'
-#data_os = pd.read_csv('../data/stake_coordinates/' + filename
-#        + '.csv', sep=' ')
-#      
-#data_os_dir = {}
-#for i in range(0, len(data_os['Name'])):
-#    data_os_dir[data_os['Name'][i]] = {'Northing': data_os['Northing'][i],\
-#    'Easting': data_os['Easting'][i], 'Elevation': data_os['Elevation'][i]}
+filename_os = '2018_stake_coordinates_corr'
+data_os = pd.read_csv('../data/stake_coordinates/' + filename_os
+        + '.csv', sep=' ')
+      
+data_os_dir = {}
+for i in range(0, len(data_os['Name'])):
+    data_os_dir[data_os['Name'][i]] = {'Northing': data_os['Northing'][i],\
+    'Easting': data_os['Easting'][i], 'Elevation': data_os['Elevation'][i]}
 
 # fieldbook data
 fieldbook = pd.read_csv('../data/fieldbook_data.csv',
@@ -34,7 +34,8 @@ fieldbook = pd.read_csv('../data/fieldbook_data.csv',
 
 # --- data to directory
 corr_dir = {}   
-fd_dir = {}                  
+fd_pos_dir = {}  
+fd_other_dir = {}                
 for k in range(0, len(fieldbook['name'])):
     # --- calculate the correction values 
     # absoute delta
@@ -53,10 +54,11 @@ for k in range(0, len(fieldbook['name'])):
     'delta_elev_trimb': delta_elev_trimb}
     
     # creating dict for the fieldbook data table     
-    fd_dir[fieldbook['name'][k]] = {'Northing [m]': '{:.2f}'.format(fieldbook['northing'][k]),
+    fd_pos_dir[fieldbook['name'][k]] = {'Northing [m]': '{:.2f}'.format(fieldbook['northing'][k]),
     'Easting [m]': '{:.2f}'.format(fieldbook['easting'][k]),
-    'Elevation [m]': round(fieldbook['elevation'][k],2),
-    'Antenna height [m]': fieldbook['antenna_height'][k], 
+    'Elevation [m]': round(fieldbook['elevation'][k],2)}
+    
+    fd_other_dir[fieldbook['name'][k]] = {'Antenna height [m]': fieldbook['antenna_height'][k], 
     'Snow depth [m]': fieldbook['snow_depth'][k],
     'Inclination [deg]': fieldbook['inclination'][k],
     'Direction of Incl. [deg]': fieldbook['inc_dir'][k],
@@ -68,15 +70,15 @@ for key in data_tbc_dir.keys():
     # corrected values for TBC
     n_tbc = data_tbc_dir[key]['Northing'] + corr_dir[key]['delta_north']
     e_tbc = data_tbc_dir[key]['Easting'] + corr_dir[key]['delta_east']
-    u_tbc = data_tbc_dir[key]['Elevation'] + corr_dir[key]['delta_elev_trimb']
+    u_tbc = data_tbc_dir[key]['Elevation'] - corr_dir[key]['delta_elev_trimb']
     final_tbc_dir[key] = {'Northing': round(n_tbc,2), 'Easting': round(e_tbc,2), \
     'Elevation': round(u_tbc,2)}
     # corrected values for TBC
-#    n_os = data_os_dir[key]['Northing'] + corr_dir[key]['delta_north']
-#    e_os = data_os_dir[key]['Easting'] + corr_dir[key]['delta_east']
-#    u_os = data_os_dir[key]['Elevation'] + corr_dir[key]['delta_elev']
-#    final_os_dir[key] = {'Northing': round(n_os,2), 'Easting': round(e_os,2), \
-#    'Elevation': round(u_os,2)}
+    n_os = data_os_dir[key]['Northing'] + corr_dir[key]['delta_north']
+    e_os = data_os_dir[key]['Easting'] + corr_dir[key]['delta_east']
+    u_os = data_os_dir[key]['Elevation'] - corr_dir[key]['delta_elev']
+    final_os_dir[key] = {'Northing': round(n_os,2), 'Easting': round(e_os,2), \
+    'Elevation': round(u_os,2)}
     
     
 # --- data to DataFrame
@@ -86,24 +88,34 @@ df_tbc_final = pd.DataFrame(final_tbc_dir).transpose().reset_index().rename(colu
 df_tbc_final = df_tbc_final[['Name', 'Northing [m]', 'Easting [m]', 'Elevation [m]']]
 tab_tbc_final = df_tbc_final.to_latex(index=False)
  
-f = open('../protocol/tables/tbc_tab.tex', 'w')
-f.write(tab_tbc_final.encode('utf-8'))
+#with open('../protocol/tables/tbc_tab.tex', 'w') as f:
+#    f.write(tab_tbc_final.encode('utf-8'))
+
+# data in .csv-file 
+#df_tbc_final.to_csv('../data/stake_coordinates/' + filename_tbc + '_final' + '.csv', sep=' ', encoding='utf-8')
 
 # open source
-#df_os_final = pd.DataFrame(final_os_dir).transpose().reset_index().rename(columns={'index':'Name',
-# 'Northing': 'Northing [m]', 'Easting': 'Easting [m]', 'Elevation': 'Elevation [m]' })
-#df_os_final = df_os_final[['Name', 'Northing [m]', 'Easting [m]', 'Elevation [m]']]
-#tab_os_final = df_os_final.to_latex(index=False)
-# 
-#f = open('../protocol/tables/os_tab.tex', 'w')
-#f.write(tab_os_final.encode('utf-8'))
+df_os_final = pd.DataFrame(final_os_dir).transpose().reset_index().rename(columns={'index':'Name',
+ 'Northing': 'Northing [m]', 'Easting': 'Easting [m]', 'Elevation': 'Elevation [m]' })
+df_os_final = df_os_final[['Name', 'Northing [m]', 'Easting [m]', 'Elevation [m]']]
+tab_os_final = df_os_final.to_latex(index=False)
+ 
+#with open('../protocol/tables/os_tab.tex', 'w') as f:
+#    f.write(tab_os_final.encode('utf-8'))
+
+#df_os_final.to_csv('../data/stake_coordinates/' + filename_os + '_final' + '.csv', sep=' ', encoding='utf-8')
 
 # fieldbook
-df_fb_final = pd.DataFrame(fd_dir).transpose().reset_index().rename(columns={'index':'Name'})
-df_fb_final = df_fb_final[['Name', 'Northing [m]', 'Easting [m]',
-'Elevation [m]', 'Antenna height [m]', 'Snow depth [m]', 'Inclination [deg]',  
+df_fb_final = pd.DataFrame(fd_pos_dir).transpose().reset_index().rename(columns={'index':'Name'})
+df_other_final = pd.DataFrame(fd_other_dir).transpose().reset_index().rename(columns={'index':'Name'})
+df_fb_final = df_fb_final[['Name', 'Northing [m]', 'Easting [m]', 'Elevation [m]']]
+df_other_final = df_other_final[['Name', 'Antenna height [m]', 'Snow depth [m]', 'Inclination [deg]',  
 'Direction of Incl. [deg]', 'Distance Rover-Stake [m]']]
 tab_fb_final = df_fb_final.to_latex(index=False)
+tab_other_final = df_other_final.to_latex(index=False)
  
-with open('../protocol/tables/fb_tab.tex', 'w') as f:
+with open('../protocol/tables/fb_pos_tab.tex', 'w') as f:
     f.write(tab_fb_final.encode('utf-8'))
+
+with open('../protocol/tables/fb_other_tab.tex', 'w') as f:
+    f.write(tab_other_final.encode('utf-8'))
