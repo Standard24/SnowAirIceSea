12f7934804efede8c5566f4d825cd2585da922b5
lindathielke <linda-thielke@gmx.de>
2018-04-09 11:50:01 +0200

    new latex template from 'students read only server'

---
+++ latex_group_report_template/make.sh
@@ -0,0 +1,24 @@
+pdflatex Main.tex -shell-escape
+bibtex Main1.aux
+#bibtex Main2.aux
+#bibtex Main3.aux
+#bibtex Main4.aux
+#bibtex Main5.aux
+#bibtex Main6.aux
+#bibtex Main7.aux
+#bibtex Main8.aux
+#bibtex Main9.aux
+#bibtex Main10.aux
+#bibtex Main11.aux
+#bibtex Main12.aux
+#bibtex Main13.aux
+#bibtex Main14.aux
+#bibtex Main15.aux
+#bibtex Main16.aux
+#bibtex Main17.aux
+#bibtex Main18.aux
+pdflatex Main.tex -shell-escape
+pdflatex Main.tex -shell-escape
+
+rm *.bbl *.log *.out *.toc
+
